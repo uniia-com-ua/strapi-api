63f673dcf67e5a5eddd0a4f2c2802d23f1687405
Artem Shyian <artem.shyian@vira.games>
2025-08-29 00:40:11 +0300

    [add] email api

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+    email: {
+        config: {
+          provider: 'strapi-provider-email-brevo',
+          providerOptions: {
+            apiKey: env('BREVO_API_KEY'),
+          },
+          settings: {
+            defaultSenderEmail: 'support@uniia.com.ua',
+            defaultSenderName: 'Унія',
+            defaultReplyTo: 'support@uniia.com.ua',
+          },
+        },
+      }
+});
